bc3668441a8f7da1d44eaf7348a3b18a8d3e6d19
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-03-19 22:18:35 -0300

    abstract  pessoa

--- clinica/src/main/java/br/unipar/clinica2/Repository/PacienteRepository.java
+++ clinica/src/main/java/br/unipar/clinica2/Repository/PacienteRepository.java
@@ -4,10 +4,49 @@
  */
 package br.unipar.clinica2.Repository;
 
+import br.unipar.clinica2.model.Paciente;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author vinid
  */
 public class PacienteRepository {
+    
+     public PacienteRepository(){
+        
+    }
+    
+    public ArrayList<Paciente> findPaciente(String nome){
+        return null;
+    }
+    
+    public ArrayList<Paciente> listAll() throws SQLException{
+        return null;
+    }
+    
+    public Paciente findById(int id){
+        return null;
+    }
+   
+    public Paciente insert(Paciente paciente) throws SQLException {
+        
+        
+        return paciente;
+    }
+
+    public Paciente update(Paciente paciente) throws SQLException {
+    
+    return paciente;
+
+    }
+    public Paciente delete(Paciente paciente) throws SQLException {
+    
+    return paciente;
+
+    }
+            
+    
     
 }
--- clinica/src/main/java/br/unipar/clinica2/services/PacientesService.java
+++ clinica/src/main/java/br/unipar/clinica2/services/PacientesService.java
@@ -4,10 +4,44 @@
  */
 package br.unipar.clinica2.services;
 
+import br.unipar.clinica2.Repository.PacienteRepository;
+import br.unipar.clinica2.model.Paciente;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author vinid
  */
 public class PacientesService {
+     public ArrayList<Paciente> listAll()throws SQLException{
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        return pacienteRepository.listAll();
+    }
+    
+    public Paciente findById(int id)throws SQLException {
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        return pacienteRepository.findById(id);
+    }
+    public ArrayList<Paciente> findPaciente(String nome) throws SQLException{
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        return pacienteRepository.findPaciente(nome);
+    }
+    
+    public Paciente insert(Paciente paciente) throws SQLException{
+        
+     
+    }
+    
+    public Paciente update(Paciente paciente) throws SQLException  {
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        return pacienteRepository.update(Paciente);
+    }
+    
+    public void delete(int id) throws SQLException{
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        pacienteRepository.delete(id);
+    }
+
     
 }
