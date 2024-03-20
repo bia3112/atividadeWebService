30d1de1f44e4b1a37dc8601112512a1e1ed4eaea
Beatriz <beatriz.omori@edu.unipar.br>
2024-03-19 21:22:14 -0300

    criado as  classes

--- clinica/src/main/java/br/unipar/clinica2/ws/PessoaWebServiceImp.java
+++ clinica/src/main/java/br/unipar/clinica2/ws/PessoaWebServiceImp.java
@@ -4,10 +4,37 @@
  */
 package br.unipar.clinica2.ws;
 
+import br.unipar.clinica2.interfaces.PessoaWebService;
+import br.unipar.clinica2.model.Pessoa;
+import jakarta.jws.WebService;
+import java.util.ArrayList;
+
 /**
  *
  * @author Beatr
  */
-public class PessoaWebServiceImp {
+
+@WebService(endpointInterface = "br.unipar.clinica2.interfaces.PessoaWebService")
+public class PessoaWebServiceImp implements PessoaWebService {
+
+    @Override
+    public Pessoa inserir(Pessoa pessoa) {
+        
+    }
+
+    @Override
+    public ArrayList<Pessoa> listAll() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Pessoa atualizar(Pessoa pessoa) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void deletar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
     
 }
