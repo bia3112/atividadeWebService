1ca0d18f279bd0771a5f53f5e4645b16343bc74b
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-03-19 22:00:05 -0300

    teste

--- clinica/src/main/java/br/unipar/clinica2/services/PacientesService.java
+++ clinica/src/main/java/br/unipar/clinica2/services/PacientesService.java
@@ -4,10 +4,42 @@
  */
 package br.unipar.clinica2.services;
 
+import br.unipar.clinica2.model.Paciente;
+import br.unipar.clinica2.repositories.PacienteRepository;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  *
  * @author vinid
  */
 public class PacientesService {
+     public ArrayList<Paciente> listAll()throws SQLException{
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        return PacienteRepository.listAll();
+    }
+    
+    public Paciente findById(int id)throws SQLException {
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        return pacienteRepository.findById(id);
+    }
+    public ArrayList<Paciente> findPaciente(String nome) throws SQLException{
+        PacienteRepository livroRepository = new PacienteRepository();
+        return PacienteRepository.findPaciente(nome);
+    }
+    
+    public Paciente insert(Paciente paciente){
+    }
+    
+    public Paciente update(Paciente paciente) throws SQLException  {
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        return pacienteRepository.update(paciente);
+    }
+    
+    public void delete(int id) throws SQLException{
+        PacienteRepository pacienteRepository = new PacienteRepository();
+        pacienteRepository.delete(id);
+    }
+
     
 }
