19f7f55f0ff58a8f57fd6194618a4b9b51dff20c
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-03-19 21:17:44 -0300

    a

---
+++ clinica/src/main/java/br/unipar/clinica2/ws/PacienteWerServicelmp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unipar.clinica2.ws;
+
+import jakarta.jws.WebService;
+
+/**
+ *
+ * @author vinid
+ */
+@WebService(endpointInterface = "br.unipar.clinica2.interfaces.")
+public class PacienteWerServicelmp {
+    
+}
