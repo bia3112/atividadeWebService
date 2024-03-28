21b7c9a1cc30ba705aad468b44123617ee200365
Vini123t <111025380+Vini123t@users.noreply.github.com>
2024-03-28 10:38:32 -0300

    testeafazer

---
+++ clinica/src/main/java/br/unipar/clinica2/Repository/ConsultaRepository.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unipar.clinica2.Repository;
+
+/**
+ *
+ * @author vinicius.duarte
+ */
+public class ConsultaRepository {
+    
+}
---
+++ clinica/src/main/java/br/unipar/clinica2/model/Consulta.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.unipar.clinica2.model;
+
+import java.time.LocalDateTime;
+    
+public class Consulta {
+    private Paciente paciente;
+    private Medico medico;
+    private LocalDateTime dataHora;
+
+    public Consulta(Paciente paciente, Medico medico, LocalDateTime dataHora) {
+        this.paciente = paciente;
+        this.medico = medico;
+        this.dataHora = dataHora;
+    }
+
+    public Consulta() {
+    }
+
+    public void setPaciente(Paciente paciente) {
+        this.paciente = paciente;
+    }
+
+    public void setMedico(Medico medico) {
+        this.medico = medico;
+    }
+
+    public void setDataHora(LocalDateTime dataHora) {
+        this.dataHora = dataHora;
+    }
+    
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public LocalDateTime getDataHora() {
+        return dataHora;
+    }
+
+    
+}
